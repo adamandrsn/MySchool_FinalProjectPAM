35dcfcc3e6fd72b64a4d1d11379294ae7970b5f1
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2024-01-10 19:47:35 +0700

    add PetaNavigasi (by Adam & Adi)

---
+++ app/src/main/java/com/example/myschool/navigasi/PetaNavigasi.kt
@@ -0,0 +1,59 @@
+package com.example.myschool.navigasi
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.myschool.R
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()) {
+    HostNavigasi(navController = navController)
+}
+
+// Fungsi SiswaTopAppBar digunakan untuk menampilkan app bar pada aplikasi
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        })
+}
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    // Mendefinisikan NavHost untuk menangani navigasi menggunakan Compose Navigation
+    NavHost(
+        navController = navController,
+        modifier = Modifier
+    ){
+
+    }
+}
