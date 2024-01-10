2650f45a5e3c35defe8f7eea9c7f9bd0b713b14c
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2024-01-10 19:15:35 +0700

    add file HomeViewModel (by Adam)

---
+++ app/src/main/java/com/example/myschool/model/HomeViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.myschool.model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myschool.data.JadwalSiswa
+import com.example.myschool.repositori.RepositoriJadwalSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriJadwalSiswa: RepositoriJadwalSiswa) : ViewModel() {
+
+    // Properti TIMEOUT_MILLIS digunakan untuk menentukan waktu timeout pada stateIn
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    // Properti homeUiState merupakan StateFlow yang akan digunakan untuk mengelola UI state pada halaman utama
+    val homeUiState: StateFlow<HomeUiState> = repositoriJadwalSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(
+            // Menyimpan state UI menggunakan stateIn dengan SharingStarted.WhileSubscribed dan timeout 5 detik
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
+}
+
+// Data class HomeUiState merepresentasikan UI state pada halaman utama
+data class HomeUiState(
+    // Properti listSiswa digunakan untuk menyimpan daftar data siswa yang akan ditampilkan pada UI
+    val listSiswa: List<JadwalSiswa> = listOf()
+)
