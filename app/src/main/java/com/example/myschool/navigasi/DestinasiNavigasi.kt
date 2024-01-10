4366ba7a58956a8eda314abe9ece705be7036df4
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2024-01-10 19:59:35 +0700

    add interface DestinasiNavigasi (by Adi)

---
+++ app/src/main/java/com/example/myschool/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,9 @@
+package com.example.myschool.navigasi
+
+interface DestinasiNavigasi {
+    //Nama unik untuk menentukan jalur untuk composable
+    val route: String
+
+    //String resource id yang berisi judul yang akan ditampilkan di layar halaman.
+    val titleRes: Int
+}
